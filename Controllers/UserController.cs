8eed9c82bb6784549ce427b59c9d96b833a08467
Dian13wonderland@gmail.com <Diana-D-13>
2023-09-29 20:37:46 +0400

    Add project files.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication20.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly List<USer> _users; 
+        public UserController()
+        {
+            _users = new List<USer>();
+            for(int i = 1;i<= 10;i++)
+            {
+                USer u = new USer()
+                {
+                    Id = i,
+                    Name = $"JAck{i}",
+                    LastName = $"Smith{i}"
+                };
+            _users.Add(u);
+            }
+        }
+        [HttpGet]
+        [Route("get-all")]
+        public IActionResult GetAllUsers() //end point 
+        {
+            var data = _users;
+            return Ok(data);
+        }
+    }
+}
---
+++ USer.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication20
+{
+    public class USer
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public string LastName { get; set; }    
+    }
+}
